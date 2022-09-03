041ed5136c6a3fc0ba9b7df6aaf1e110441ba722
taisei_ueki <dachengzhimu044@gmail.com>
2022-09-03 13:54:55 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about", as: 'about'
+  
+  devise_for:users,controllers:{
+    registrations: "registrations",
+    sessions: 'sessions'
+  }
+  
+  devise_for:admin,controllers:{
+    sessions: "admin/sessions"
+  }
+  
+  resources :users, only:[:show, :edit, :update]
+  
+  resources :items, only:[:index, :show]
+  
+  resources :cart_items, only:[:index, :create, :update, :destroy] do
+    collection do
+      delete :destroy_all
+    end
+  end
+  
+  resources :order, only:[:new, :index, :create, :show] do
+    collection do
+      get   :confrim
+      patch :complete
+    end
+  end
+  
+  resources :genres, only:[:show]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
