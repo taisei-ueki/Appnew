8f71bff254accc0f97c147b928404e9ddb16ba9b
taisei_ueki <dachengzhimu044@gmail.com>
2022-09-05 14:10:08 +0000

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,19 @@
 class ItemsController < ApplicationController
+  
+  def index
+    @items = Item.all
+    @genres = Genre.all
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+    @cart_item = CartItem.new
+  end
+  
+  private
+  def item_params
+    params.require(:user).permit(:name,:price,:introduction)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = current_user
+  end
+  
+  def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+  
 end
