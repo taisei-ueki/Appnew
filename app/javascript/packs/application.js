906a5822ca9e756359e4afe58adf80e1eec4be16
taisei_ueki <dachengzhimu044@gmail.com>
2022-09-20 12:17:43 +0000

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,6 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application" 
 import '@fortawesome/fontawesome-free/js/all'
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
